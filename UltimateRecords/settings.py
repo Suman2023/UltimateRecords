e337c6b10e569bfbd4bef6dcf8f85643dedfa93b
Suman <66205793+Suman2023@users.noreply.github.com>
2021-08-04 21:54:46 +0530

    Updated secret key

--- UltimateRecords/settings.py
+++ UltimateRecords/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w^@6h$35_+=-9d=ms_hw)i@!x=p0pp8jxgz!4%=yt^i8k*5+mb'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
